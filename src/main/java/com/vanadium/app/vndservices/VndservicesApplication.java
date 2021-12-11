e374423f65bb0b69bf6614b471168637a4ecc05f
unknown <rathoreharshvardhansingh1997@gmail.com>
2021-12-11 15:14:50 +0530

    Setting up the backend service

---
+++ src/main/java/com/vanadium/app/vndservices/VndservicesApplication.java
@@ -0,0 +1,13 @@
+package com.vanadium.app.vndservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VndservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VndservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vanadium/app/vndservices/VndservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanadium.app.vndservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VndservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
